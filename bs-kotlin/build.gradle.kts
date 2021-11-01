17818893cf2016343c3c1c9f5f0ca0ef50fe1856
Daz DeBoer <daz@gradle.com>
2021-11-01 14:34:41 -0600

    Tag subprojects in allprojects block

--- bs-kotlin/build.gradle.kts
+++ bs-kotlin/build.gradle.kts
@@ -3,3 +3,7 @@ buildScan.tag("ROOTPROJECT_BUILD")
 project("app") {
     rootProject.buildScan.tag("SUBPROJECT_FROM_ROOT")
 }
+
+allprojects {
+    rootProject.buildScan.tag("PROJECT-" + this.name)
+}
