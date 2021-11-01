52323474e08b1283f3863e051e8df225a96e72e9
Daz DeBoer <daz@gradle.com>
2021-11-01 13:51:43 -0600

    Initial commit

---
+++ bs-kotlin/settings.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.gradle.enterprise") version("3.7.1")
+}
+gradleEnterprise {
+    server = "https://localhost"
+    allowUntrustedServer = true
+    buildScan {
+        publishAlways()
+    }
+}
+
+rootProject.name = "bs-kotlin"
+include("app", "list", "utilities")
